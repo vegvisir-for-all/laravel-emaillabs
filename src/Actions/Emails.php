5005831f560c48d0516fa5ecc59c39cd0ffe05da
Marek Kapusta-Ognicki <marek@vegvisir.online>
2019-02-06 17:42:29 +0100

    Added emails endpoint

--- src/Actions/Emails.php
+++ src/Actions/Emails.php
@@ -1,1 +1,73 @@
 <?php
+
+/*
+ * This file is part of the Vegvisir's Laravel EmailLabs package.
+ * It provides a Laravel wrapper for EmailLabs Library.
+ *
+ * @copyright 2019 Vegvisir Sp. z o.o. <vegvisir.for.all@gmail.com>
+ * @license MIT
+ */
+
+namespace Vegvisir\EmailLabs\Actions;
+
+use Vegvisir\EmailLabs\Actions\Action as BaseAction;
+
+class Emails extends BaseAction
+{
+    /**
+     * Request method.
+     *
+     * @var string
+     */
+    protected $method = 'GET';
+
+    /**
+     * API endpoint.
+     *
+     * @var string
+     */
+    protected $action = 'emails';
+
+    /**
+     * Filters array.
+     *
+     * @var array
+     */
+    protected $filters = [];
+
+    /**
+     * Params array.
+     *
+     * @var array
+     */
+    protected $params = [
+        'offset' => 0,
+        'limit' => 100,
+        'sort' => '-created_at',
+    ];
+
+    /**
+     * Allowed filters.
+     *
+     * @var array
+     */
+    protected $allowFilters = [
+        'account',
+        'message_id',
+        'created_at.from',
+        'created_at.to',
+        'injected_time.from',
+        'injected_time.to'
+    ];
+
+    /**
+     * Allowed params.
+     *
+     * @var array
+     */
+    protected $allowParams = [
+        'sort',
+        'limit',
+        'offset',
+    ];
+}
